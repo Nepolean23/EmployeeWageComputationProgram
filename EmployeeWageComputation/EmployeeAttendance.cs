f47ad1403cbbbad3e0e6d23c13f989d57f92bbc6
Nepolean23 <nlean1996@gmail.com>
2023-01-03 18:54:38 +0530

    [Nepolean]-EmployeeAttendance Checking

---
+++ EmployeeWageComputation/EmployeeAttendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    public class EmployeeAttendance
+    {
+        public static void Attendance()
+        {
+            //Constants
+            int Is_Full_Time = 1;
+            Random random = new Random();
+            //Computation
+            int empcheck = random.Next(0, 2);
+            if (empcheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
